882f805477d23976b0d0b5bbab5fa55943436979
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-16 16:43:53 +0300

    Update HttpClient.php

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -44,19 +44,4 @@ class HttpClient implements ClientInterface
     {
         $this->defaultOpts = array_merge($this->defaultOpts, $defaultOpts);
     }
-
-    /**
-     * Make an HTTP request.
-     *
-     * @param  string  $method  The request method.
-     * @param  \Psr\Http\Message\UriInterface|string  $uri  The request URI.
-     * @param  array  $opts  The array of request options.
-     * @return \Psr\Http\Message\ResponseInterface
-     *
-     * @throws \Psr\Http\Client\ClientExceptionInterface
-     */
-    public function request($method = Method::GET, $uri = '/', array $opts = []): ResponseInterface
-    {
-        //
-    }
 }
