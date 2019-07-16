841ef4b73fc7bbe6784847e34110b889f579040d
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-16 17:10:57 +0300

    Some fixes

---
+++ src/Middleware/PrepareRequestMiddleware.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Lazy\Http\Middleware;
+
+use Lazy\Http\Contracts\HttpMiddlewareInterface;
+
+class PrepareRequestMiddleware implements HttpMiddlewareInterface
+{
+    
+}
---
+++ src/Middleware/PrepareResponseMiddleware.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Lazy\Http\Middleware;
+
+use Lazy\Http\Contracts\HttpMiddlewareInterface;
+
+class PrepareResponseMiddleware implements HttpMiddlewareInterface
+{
+    
+}
