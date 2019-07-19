62f40c2274c0733014c3c93778acb608ea7e49e4
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-19 20:05:43 +0300

    Update Builder.php

--- src/Builder.php
+++ src/Builder.php
@@ -2,6 +2,8 @@
 
 namespace Lazy\Db;
 
+use InvalidArgumentException;
+
 class Builder
 {
     /**
@@ -22,4 +24,26 @@ class Builder
         'union' => []
 
     ];
+
+    public function where(array $where, $bolean = 'and')
+    {
+        if (1 === count($where)) {
+            [$column, $operator, $value] = [$where[0], 'IS NOT', 'NULL'];
+        } else if (2 === count($where)) {
+            [$column, $operator, $value] = [$where[0], '=', $where[1]];
+        } else if (3 === count($where)) {
+            [$column, $operator, $value] = [$where[0], $where[1], $where[2]];
+        } else {
+            throw new InvalidArgumentException('Invalid "where(...)" arguments!');
+        }
+
+        $this->parts['where'] = compact('column', 'operator', 'value', 'boolean');
+
+        return $this;
+    }
+
+    public function orWhere(array $where)
+    {
+        return $this->where($where, 'or');
+    }
 }
