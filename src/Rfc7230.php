f6517b5d0ec4fe4bce534a60ca3c9a3dda07cc22
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-19 15:51:56 +0300

    Update Rfc7230.php

--- src/Rfc7230.php
+++ src/Rfc7230.php
@@ -7,5 +7,21 @@ namespace Lazy\Http;
  */
 abstract class Rfc7230
 {
-    
+    /**
+     * @var string
+     *
+     * @see https://tools.ietf.org/html/rfc7230#section-3.2.6
+     */
+    public static $token = '[!#$%&\'*+\-.^_`|~0-9A-Za-z]+';
+
+    /**
+     * Check is the header field name valid.
+     *
+     * @param  string  $name  The header field name.
+     * @return bool
+     */
+    public static function isHeaderFieldNameValid($name)
+    {
+        return preg_match('/^'.static::$token.'$/', $name);
+    }
 }
