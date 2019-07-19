38644a84f9b5359740f6f38f09ba13b5f2c61af1
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-19 12:37:00 +0300

    Update Rfc3986.php

--- src/Rfc3986.php
+++ src/Rfc3986.php
@@ -12,33 +12,33 @@ abstract class Rfc3986
      *
      * @see https://tools.ietf.org/html/rfc3986#section-2.1
      */
-    protected static $pctEncoded = '\%[A-Fa-f0-9]{2}';
+    public static $pctEncoded = '\%[A-Fa-f0-9]{2}';
 
     /**
      * @var string
      *
      * @see https://tools.ietf.org/html/rfc3986#section-2.2
      */
-    protected static $genDelims = ':\/?#[]@';
+    public static $genDelims = ':\/?#[]@';
 
     /**
      * @var string
      *
      * @see https://tools.ietf.org/html/rfc3986#section-2.2
      */
-    protected static $subDelims = '!$&\'()*+,;=';
+    public static $subDelims = '!$&\'()*+,;=';
 
     /**
      * @var string
      *
      * @see https://tools.ietf.org/html/rfc3986#section-2.3
      */
-    protected static $unreserved = 'A-Za-z0-9\-._~';
+    public static $unreserved = 'A-Za-z0-9\-._~';
 
     /**
      * @var string
      *
      * @see https://tools.ietf.org/html/rfc3986#section-3.1
      */
-    protected static $schemePattern = '/^[a-z][a-z0-9+\-.]*$/i';
+    public static $schemePattern = '/^[a-z][a-z0-9+\-.]*$/i';
 }
