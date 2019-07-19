36176d9e6a5e806a130db51bfdf05df05b06b57a
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-19 13:28:46 +0300

    Update Rfc3986.php

--- src/Rfc3986.php
+++ src/Rfc3986.php
@@ -52,4 +52,95 @@ abstract class Rfc3986
     {
         return preg_match(static::$schemePattern, $scheme);
     }
+
+    /**
+     * Check is the user information component of the URI valid.
+     *
+     * @param  string  $userInfo  The user information component of the URI.
+     * @return bool
+     */
+    public static function isUserInfoValid($userInfo)
+    {
+        return preg_match('/^(['.static::$unreserved.static::$subDelims.':]|'.static::$pctEncoded.')*$/', $userInfo);
+    }
+
+    /**
+     * Check is the host component of the URI valid.
+     *
+     * @param  string  $host  The host component of the URI.
+     * @return bool
+     */
+    public function isHostValid($host)
+    {
+        return static::isIpVFutureValid($host) ||
+            static::isIpV4AddressValid($host) || static::isIpV6AddressValid($host) ||
+            preg_match('/^(['.static::$unreserved.static::$subDelims.']|'.static::$pctEncoded.')*$/', $host);
+    }
+
+    /**
+     * Check is the "IPvFuture" of the host component of the URI valid.
+     *
+     * @param  string  $ip  The "IPvFuture" of the host component of the URI
+     * @return bool
+     */
+    public static function isIpVFutureValid($ip)
+    {
+        return preg_match('/^\[v[A-Fa-f0-9]\.['.static::$unreserved.static::$subDelims.':]\]$/i', $ip);
+    }
+
+    /**
+     * Check is the "IPv4address" of the host component of the URI valid.
+     *
+     * @param  string  $ip  The "IPv4address" of the host component of the URI
+     * @return bool
+     */
+    public static function isIpV4AddressValid($ip)
+    {
+        return false !== filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+    }
+
+    /**
+     * Check is the "IPv6address" of the host component of the URI valid.
+     *
+     * @param  string  $ip  The "IPv6address" of the host component of the URI
+     * @return bool
+     */
+    public static function isIpV6AddressValid($ip)
+    {
+        return '[' === $ip[0] && ']' === $ip[strlen($ip) - 1] && false !== filter_var(trim($ip, '[]'), FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+    }
+
+    /**
+     * Check is the port component of the URI valid.
+     *
+     * @param  int  $port  The port component of the URI.
+     * @return bool
+     */
+    public function isPortValid($port)
+    {
+        return 0 < $port && 65536 > $port;
+    }
+
+    /**
+     * Check is the path component of the URI valid.
+     *
+     * @param  string  $path  The path component of the URI.
+     * @param  bool  $authority  Has the URI an authority component.
+     * @return bool
+     */
+    public function isPathValid($path, $authority = false)
+    {
+        
+    }
+
+    /**
+     * Check is the query or fragment component of the URI valid.
+     *
+     * @param  string  $queryOrFragment  The query or fragment component of the URI.
+     * @return bool
+     */
+    public function isQueryOrFragmentValid($queryOrFragment)
+    {
+        return preg_match('/^(['.static::$unreserved.static::$subDelims.':@\/?]|'.static::$pctEncoded.')*$/', $queryOrFragment);
+    }
 }
