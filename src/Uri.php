191913588cbc6f3d1b853c4ff69fc3581575cf5c
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-22 18:48:58 +0300

    Create Uri.php

---
+++ src/Uri.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace Lazy\Http;
+
+use InvalidArgumentException;
+use Psr\Http\Message\UriInterface;
+
+class Uri implements UriInterface
+{
+    /**
+     * The scheme component of the URI.
+     *
+     * @var string
+     */
+    protected $scheme = '';
+
+    /**
+     * The user information component of the URI.
+     *
+     * @var string
+     */
+    protected $userInfo = '';
+
+    /**
+     * The host component of the URI.
+     *
+     * @var string
+     */
+    protected $host = '';
+
+    /**
+     * The port component of the URI.
+     *
+     * @var int|null
+     */
+    protected $port;
+
+    /**
+     * The path component of the URI.
+     *
+     * @var string
+     */
+    protected $path = '';
+
+    /**
+     * The query component of the URI.
+     *
+     * @var string
+     */
+    protected $query = '';
+
+    /**
+     * The fragment component of the URI.
+     *
+     * @var string
+     */
+    protected $fragment = '';
+
+    /**
+     * The array of standart TCP or UDP ports.
+     *
+     * @var array
+     */
+    protected static $standartPorts = ['http' => 80, 'https' => 443];
+
+    /**
+     * The "RFC 3986" sub-delimiters.
+     *
+     * @var string
+     */
+    protected static $subDelims = '!$&\'()*+,;=';
+
+    /**
+     * The "RFC 3986" unreserved characters.
+     *
+     * @var string
+     */
+    protected static $unreserved  = 'A-Za-z0-9\-._~';
+
+    /**
+     * Create a new URI instance.
+     *
+     * @param string $uri The URI string.
+     *
+     * @throws \InvalidArgumentException If unable to parse
+     *      the URI string or a component of the URI is not valid.
+     */
+    public function __construct(string $uri = '')
+    {
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getScheme()
+    {
+        return strtolower($this->scheme);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAuthority()
+    {
+        $userInfo = $this->getUserInfo();
+        $host = $this->getHost();
+        $port = $this->getPort();
+
+        $authority = $host;
+
+        if ($authority) {
+            if ($userInfo) {
+                $authority = $userInfo.'@'.$authority;
+            }
+
+            if (null !== $port) {
+                $authority .= ':'.$port;
+            }
+        }
+
+        return $authority;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getUserInfo()
+    {
+        return $this->userInfo;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getHost()
+    {
+        return strtolower($this->host);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getPort()
+    {
+        if (static::isStandartPortForScheme($this->port, $this->scheme)) {
+            return null;
+        }
+
+        return $this->port;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getPath()
+    {
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getQuery()
+    {
+        
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFragment()
+    {
+        
+    }
+
+    /**
+     * Is the TCP or UDP port standart
+     * for the given scheme component of the URI.
+     *
+     * @param int $port The TCP or UDP port.
+     * @param string $scheme The scheme component of the URI.
+     *
+     * @return bool Returns true
+     *      if the TCP or UDP port standart
+     *      for the given scheme component of the URI.
+     */
+    public static function isStandartPortForScheme($port, $scheme = 'http')
+    {
+        return isset(static::$standartPorts[$scheme]) && $port === static::$standartPorts[$scheme];
+    }
+}
