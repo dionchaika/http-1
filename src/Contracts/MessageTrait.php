27d0039f72d75051c6674c47f5f2e464e54f5363
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-23 18:09:04 +0300

    Update MessageTrait.php

--- src/Contracts/MessageTrait.php
+++ src/Contracts/MessageTrait.php
@@ -2,124 +2,19 @@
 
 namespace Lazy\Http\Contracts;
 
-use InvalidArgumentException;
-use Psr\Http\Message\StreamInterface;
-
 trait MessageTrait
 {
     /**
-     * The "RFC 7230" header field-name.
+     * The "RFC 7230" header field name.
      *
      * @var string
      */
-    protected static $fieldName = '/^[!#$%&\'*+\-.^_`|~0-9A-Za-z]+$/';
+    protected static $headerFieldName = '/^[!#$%&\'*+\-.^_`|~0-9A-Za-z]+$/';
 
     /**
-     * The "RFC 7230" header field-value.
+     * The "RFC 7230" header field value.
      *
      * @var string
      */
-    protected static $fieldValue = '/^[ \t]*(?:(?:[\x21-\x7e\x80-\xff](?:[ \t]+[\x21-\x7e\x80-\xff])?)|\r?\n[ \t]+)*[ \t]*$/';
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getProtocolVersion()
-    {
-        return $this->protocolVersion;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function withProtocolVersion($version)
-    {
-        $new = clone $this;
-        $new->protocolVersion = $version;
-
-        return $new;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getHeaders()
-    {
-        $headers = [];
-
-        foreach ($this->headers as $header) {
-            $headers[$header['name']] = $header['values'];
-        }
-
-        return $headers;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function hasHeader($name)
-    {
-        return isset($this->headers[strtolower($name)]);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getHeader($name)
-    {
-        $name = strtolower($name);
-
-        return isset($this->headers[$name]) ? $this->headers[$name]['values'] : [];
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getHeaderLine($name)
-    {
-        return implode(', ', $this->getHeader($name));
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function withHeader($name, $value)
-    {
-        
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function withAddedHeader($name, $value)
-    {
-        
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function withoutHeader($name)
-    {
-        $new = clone $this;
-        unset($new->headers[strtolower($name)]);
-
-        return $new;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function getBody()
-    {
-        
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function withBody(StreamInterface $body)
-    {
-        
-    }
+    protected static $headerFieldValue = '/^[ \t]*(?:(?:[\x21-\x7e\x80-\xff](?:[ \t]+[\x21-\x7e\x80-\xff])?)|\r?\n[ \t]+)*[ \t]*$/';
 }
