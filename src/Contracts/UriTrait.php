43c015ba8149f57bbb141ceb2d50038a34ff58ec
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-23 15:52:13 +0300

    Update UriTrait.php

--- src/Contracts/UriTrait.php
+++ src/Contracts/UriTrait.php
@@ -2,101 +2,42 @@
 
 namespace Lazy\Http\Contracts;
 
-use Lazy\Http\Uri;
 use Psr\Http\Message\UriInterface;
 
 trait UriTrait
 {
-    public static function removeDotSegments($path)
-    {
-        $input = $path;
-        $output = [];
-
-        while ('' !== $input) {
-            /**
-             * A.  If the input buffer begins with a prefix of "../" or "./",
-             *     then remove that prefix from the input buffer; otherwise,
-             */
-            if (0 === strpos($input, './')) {
-                $input = substr($input, 2);
-                continue;
-            }
-            if (0 === strpos($input, '../')) {
-                $input = substr($input, 3);
-                continue;
-            }
-
-            /**
-             * B.  if the input buffer begins with a prefix of "/./" or "/.",
-             *     where "." is a complete path segment, then replace that
-             *     prefix with "/" in the input buffer; otherwise,
-             */
-            if ('/.' === $input) {
-                $output[] = '/';
-                break;
-            }
-            if (0 === strpos($input, '/./')) {
-                $input = substr($input, 2);
-                continue;
-            }
-
-            /**
-             * C.  if the input buffer begins with a prefix of "/../" or "/..",
-             *     where ".." is a complete path segment, then replace that
-             *     prefix with "/" in the input buffer and remove the last
-             *     segment and its preceding "/" (if any) from the output
-             *     buffer; otherwise,
-             */
-            if ('/..' === $input) {
-                array_pop($output);
-                $output[] = '/';
-
-                break;
-            }
-            if (0 === strpos($input, '/../')) {
-                array_pop($output);
-                $input = substr($input, 3);
-
-                continue;
-            }
-
-            /**
-             * D.  if the input buffer consists only of "." or "..", then remove
-             *     that from the input buffer; otherwise,
-             */
-            if ('.' === $input || '..' === $input) {
-                break;
-            }
-
-            /**
-             * E.  move the first path segment in the input buffer to the end of
-             *     the output buffer, including the initial "/" character (if
-             *     any) and any subsequent characters up to, but not including,
-             *     the next "/" character or the end of the input buffer.
-             */
-            if (false === $slashPos = strpos($input, '/', 1)) {
-                $output[] = $input;
-                break;
-            } else {
-                $output[] = substr($input, 0, $slashPos);
-                $input = substr($input, $slashPos);
-            }
-        }
-
-        return implode($output);
-    }
+    /**
+     * The "RFC 3986" sub-delimiters.
+     *
+     * @var string
+     */
+    protected static $subDelims = '!$&\'()*+,;=';
 
     /**
-     * Transform a URI reference to its target URI against the base URI.
+     * The "RFC 3986" unreserved characters.
      *
-     * @param UriInterface $base The base URI.
-     * @param UriInterface $reference The URI reference.
-     * @param bool $strict Use strict mode.
-     *
-     * @return UriInterface The target URI.
+     * @var string
      */
-    public static function transform(UriInterface $base, UriInterface $reference, $strict = true)
+    protected static $unreserved = 'A-Za-z0-9\-._~';
+
+    /**
+     * The array of standart TCP and UDP ports.
+     *
+     * @var array
+     */
+    protected static $standartPorts = ['http' => 80, 'https' => 443];
+
+    /**
+     * Is the TCP or UDP port
+     * standart for the given scheme component of the URI.
+     *
+     * @param int $port The TCP or UDP port.
+     * @param string $scheme The scheme component of the URI.
+     *
+     * @return bool
+     */
+    public static function isStandartPort($port, $scheme)
     {
-        
+        return isset(static::$standartPorts[$scheme]) && $port === static::$standartPorts[$scheme];
     }
 }
