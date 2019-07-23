d2f2bc761ad2d243dac128bb4df327dd216ae33c
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-23 18:29:09 +0300

    Update MessageTrait.php

--- src/Contracts/MessageTrait.php
+++ src/Contracts/MessageTrait.php
@@ -17,32 +17,4 @@ trait MessageTrait
      * @var string
      */
     protected static $headerFieldValue = '/^[ \t]*(?:(?:[\x21-\x7e\x80-\xff](?:[ \t]+[\x21-\x7e\x80-\xff])?)|\r?\n[ \t]+)*[ \t]*$/';
-
-    /**
-     * The array of message headers.
-     *
-     * @var array
-     */
-    protected $headers = [];
-
-    /**
-     * Is a header field valid.
-     *
-     * @param string $name A header field name.
-     * @param array $values An array of header filed values.
-     *
-     * @return bool
-     */
-    protected static function isHeaderFieldValid($name, array $values)
-    {
-        if (! preg_match(static::$headerFieldName, $name)) {
-            return false;
-        }
-
-        foreach ($values as $value) {
-            if (! preg_match(static::$headerFieldValue, $value)) {
-                return false;
-            }
-        }
-    }
 }
