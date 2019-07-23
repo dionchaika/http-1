9b6d1c183a9516c6fa6f3660a7bb7bb8a6bdcd48
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-23 13:38:44 +0300

    Update Uri.php

--- src/Uri.php
+++ src/Uri.php
@@ -20,6 +20,84 @@ class Uri implements UriInterface
 
     protected static $standartPorts = ['http' => 80, 'https' => 443];
 
+    public function __construct(string $uri = '')
+    {
+        $components = parse_url($uri);
+
+        if (false === $components) {
+            throw new InvalidArgumentException("Unable to parse the URI string: {$uri}!");
+        }
+
+        $userInfo = $this->composeUserInfo(
+            ! empty($components['user']) ? $components['user'] : '',
+            ! empty($components['pass']) ? $components['pass'] : null
+        );
+
+        $this->applyComponent('scheme', ! empty($components['scheme']) ? $components['scheme'] : '');
+        $this->applyComponent('userInfo', $userInfo);
+        $this->applyComponent('host', ! empty($components['host']) ? $components['host'] : '');
+        $this->applyComponent('port', ! empty($components['port']) ? $components['port'] : null);
+        $this->applyComponent('path', ! empty($components['path']) ? $components['path'] : '');
+        $this->applyComponent('query', ! empty($components['query']) ? $components['query'] : '');
+        $this->applyComponent('fragment', ! empty($components['fragment']) ? $components['fragment'] : '');
+    }
+
+    public function getScheme()
+    {
+        return strtolower($this->scheme);
+    }
+
+    public function getAuthority()
+    {
+        $userInfo = $this->getUserInfo();
+        $host = $this->getHost();
+        $port = $this->getPort();
+
+        $authority = $host;
+
+        if ('' !== $authority) {
+            if ('' !== $userInfo) {
+                $authority = $userInfo.'@'.$authority;
+            }
+
+            if (null !== $port) {
+                $authority .= ':'.$port;
+            }
+        }
+
+        return $authority;
+    }
+
+    public function getUserInfo()
+    {
+        return $this->userInfo;
+    }
+
+    public function getHost()
+    {
+        return strtolower($this->host);
+    }
+
+    public function getPort()
+    {
+        return static::isStandartPortForScheme($this->port, $this->scheme) ? null : $this->port;
+    }
+
+    public function getPath()
+    {
+        return $this->encodePath($this->path);
+    }
+
+    public function getQuery()
+    {
+        return $this->encodeQueryOrFragment($this->query);
+    }
+
+    public function getFragment()
+    {
+        return $this->encodeQueryOrFragment($this->fragment);
+    }
+
     public function withScheme($scheme)
     {
         $new = clone $this;
@@ -83,6 +161,37 @@ class Uri implements UriInterface
         return $new;
     }
 
+    public function __toString()
+    {
+        $scheme = $this->getScheme();
+        $authority = $this->getAuthority();
+        $path = $this->getPath();
+        $query = $this->getQuery();
+        $fragment = $this->getFragment();
+
+        $uri = '';
+
+        if ('' !== $scheme) {
+            $uri .= $scheme.':';
+        }
+
+        if ('' !== $authority || 'file' === $scheme) {
+            $uri .= '//'.$authority;
+        }
+
+        $uri .= '/'.ltrim($path, '/');
+
+        if ('' !== $query) {
+            $uri .= '?'.$query;
+        }
+
+        if ('' !== $fragment) {
+            $uri .= '#'.$fragment;
+        }
+
+        return $uri;
+    }
+
     protected function composeUserInfo($user, $password = null)
     {
         return ('' !== $user && null !== $password && '' !== $password) ? $user.':'.$password : $user;
@@ -131,6 +240,28 @@ class Uri implements UriInterface
     protected function throwInvalidComponentException($name, $value)
     {
         throw new InvalidArgumentException("The {$name} component of the URI is not valid: {$value}!");
+    }
+
+    protected function encodePath($path)
+    {
+        return preg_replace_callback(
+            '/(?:[^\/'.self::UNRESERVED.self::SUB_DELIMS.':@%]++|\%(?![A-Fa-f0-9]{2}))/',
+            function ($matches) {
+                return rawurlencode($matches[0]);
+            },
+            $path
+        );
+    }
+
+    protected function encodeQueryOrFragment($queryOrFragment)
+    {
+        return preg_replace_callback(
+            '/(?:[^\/'.self::UNRESERVED.self::SUB_DELIMS.':@?%]++|\%(?![A-Fa-f0-9]{2}))/',
+            function ($matches) {
+                return rawurlencode($matches[0]);
+            },
+            $queryOrFragment
+        );
     }
 
     public static function isStandartPortForScheme($port, $scheme)
