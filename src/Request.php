056e1d2a5e57004cb4db56f11c375ee97f608884
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-23 19:43:43 +0300

    Update Request.php

--- src/Request.php
+++ src/Request.php
@@ -33,13 +33,49 @@ class Request implements RequestInterface
     protected $requestTarget;
 
     /**
+     * The array of standart HTTP methods.
+     *
+     * @var array
+     */
+    protected static $standartMethods = [
+
+        'GET',
+        'HEAD',
+        'POST',
+        'PUT',
+        'DELETE',
+        'CONNECT',
+        'OPTIONS',
+        'TRACE'
+
+    ];
+
+    /**
+     * Create a new request instance.
+     *
+     * @param string $method Request HTTP method.
+     * @param UriInterface|string $uri Request URI instance.
+     *
+     * @throws InvalidArgumentException
+     */
+    public function __construct(string $method, $uri)
+    {
+        $this->validateMethod($method);
+
+        $this->uri = $uri;
+        $this->method = $method;
+
+        $this->setHostHeaderFromUri($uri);
+    }
+
+    /**
      * Set "Host" header to the request from the URI.
      *
      * @param UriInterface $uri The URI instance.
      *
      * @return void
      */
-    protected function setHostHeader(UriInterface $uri)
+    protected function setHostHeaderFromUri(UriInterface $uri)
     {
         $host = $uri->getHost();
 
@@ -52,5 +88,35 @@ class Request implements RequestInterface
 
             $this->headers = ['host' => ['name' => 'Host', 'values' => [$host]]] + $this->headers;
         }
+    }
+
+    /**
+     * Validate a request HTTP method.
+     *
+     * @param string $method Request HTTP method.
+     *
+     * @return void
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function validateMethod($method)
+    {
+        if (! static::isMethodValid($method)) {
+            throw new InvalidArgumentException(
+                "Request HTTP method is not valid: {$method}!"
+            );
+        }
+    }
+
+    /**
+     * Is a request HTTP method valid.
+     *
+     * @param string $method Request HTTP method.
+     *
+     * @return bool
+     */
+    protected static function isMethodValid($method)
+    {
+        return in_array($method, static::$standartMethods) || preg_match(static::$headerName, $method);
     }
 }
