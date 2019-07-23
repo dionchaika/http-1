436b02eb5d4b18004077f84b21b52c1e2892cda3
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-23 11:49:06 +0300

    Update Uri.php

--- src/Uri.php
+++ src/Uri.php
@@ -7,5 +7,79 @@ use Psr\Http\Message\UriInterface;
 
 class Uri implements UriInterface
 {
-    
+    /**
+     * The scheme component of the URI.
+     *
+     * @var string
+     */
+    protected $scheme = '';
+
+    /**
+     * The user component of the URI.
+     *
+     * @var string
+     */
+    protected $user = '';
+
+    /**
+     * The password component of the URI.
+     *
+     * @var string|null
+     */
+    protected $password;
+
+    /**
+     * The host component of the URI.
+     *
+     * @var string
+     */
+    protected $host = '';
+
+    /**
+     * The port component of the URI.
+     *
+     * @var int|null
+     */
+    protected $port;
+
+    /**
+     * The path component of the URI.
+     *
+     * @var string
+     */
+    protected $path = '';
+
+    /**
+     * The query component of the URI.
+     *
+     * @var string
+     */
+    protected $query = '';
+
+    /**
+     * The fragment component of the URI.
+     *
+     * @var string
+     */
+    protected $fragment = '';
+
+    /**
+     * The array of standart TCP and UDP ports.
+     *
+     * @var array
+     */
+    protected static $standartPorts = ['http' => 80, 'https' => 443];
+
+    /**
+     * Is the TCP or UDP port standart for the given scheme component of the URI.
+     *
+     * @param int $port The TCP or UDP port.
+     * @param string $scheme The scheme component of the URI.
+     *
+     * @return bool
+     */
+    public static function isStandartPortForScheme($port, $scheme)
+    {
+        return isset(static::$standartPorts) && $port === static::$standartPorts[$scheme];
+    }
 }
