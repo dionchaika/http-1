97337c6212df84350c6e6b2085d6a924ac7fed24
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-23 16:50:59 +0300

    Update UriTrait.php

--- src/Contracts/UriTrait.php
+++ src/Contracts/UriTrait.php
@@ -53,7 +53,17 @@ trait UriTrait
      */
     protected static function validateSchemeComponent($scheme)
     {
+        if ('' === $scheme) {
+            return $scheme;
+        }
 
+        if (! preg_match('/^[A-Za-z][A-Za-z0-9+\-.]*$/', $scheme)) {
+            throw new InvalidArgumentException(
+                "The scheme component of the URI is not valid: {$scheme}!"
+            );
+        }
+
+        return $scheme;
     }
 
     /**
@@ -67,7 +77,17 @@ trait UriTrait
      */
     protected static function validateUserInfoComponent($userInfo)
     {
-        
+        if ('' === $userInfo) {
+            return $userInfo;
+        }
+
+        if (! preg_match('/^(?:['.static::$unreserved.static::$subDelims.':]|\%[A-Fa-f0-9]{2})*$/', $userInfo)) {
+            throw new InvalidArgumentException(
+                "The user information component of the URI is not valid: {$userInfo}!"
+            );
+        }
+
+        return $userInfo;
     }
 
     /**
@@ -95,7 +115,30 @@ trait UriTrait
      */
     protected static function validatePortComponent($port)
     {
-        
+        if (null === $port) {
+            return $port;
+        }
+
+        $opts = [
+
+            'options' => [
+
+                'min_range' => 1,
+                'max_range' => 65535
+
+            ],
+            'flags' => FILTER_FLAG_ALLOW_HEX | FILTER_FLAG_ALLOW_OCTAL
+
+        ];
+
+        if (false === filter_var($port, FILTER_VALIDATE_INT, $opts)) {
+            throw new InvalidArgumentException(
+                "The port component of the URI is not valid: {$port}! "
+                ."TCP or UDP port must be in the range from 1 to 65535."
+            );
+        }
+
+        return $port;
     }
 
     /**
@@ -110,7 +153,43 @@ trait UriTrait
      */
     protected static function validatePathComponent($path, UriInterface $uri = null)
     {
-        
+        if ('' === $path) {
+            return $path;
+        }
+
+        if (null !== $uri) {
+            $scheme = $uri->getScheme();
+            $authority = $uri->getAuthority();
+
+            if ('' !== $scheme && ':' === $path[0]) {
+                throw new InvalidArgumentException(
+                    "The path component of the URI is not valid: {$path}! "
+                    ."Path of a URI with a scheme component must not begin with a colon."
+                );
+            }
+
+            if ('' !== $authority && '/' !== $path[0]) {
+                throw new InvalidArgumentException(
+                    "The path component of the URI is not valid: {$path}! "
+                    ."Path of a URI with an authority must be empty or begin with a slash."
+                );
+            }
+
+            if ('' === $authority && 0 === strpos($path, '//')) {
+                throw new InvalidArgumentException(
+                    "The path component of the URI is not valid: {$path}! "
+                    ."Path of a URI without an authority must not begin with more than one slash."
+                );
+            }
+        }
+
+        if (! preg_match('/^(?:[\/'.static::$unreserved.static::$subDelims.':@]|\%[A-Fa-f0-9]{2})*$/', $path)) {
+            throw new InvalidArgumentException(
+                "The path component of the URI is not valid: {$path}!"
+            );
+        }
+
+        return $path;
     }
 
     /**
@@ -124,6 +203,16 @@ trait UriTrait
      */
     protected static function validateQueryOrFragmentComponent($queryOrFragment)
     {
-        
+        if ('' === $queryOrFragment) {
+            return $queryOrFragment;
+        }
+
+        if (! preg_match('/^(?:[\/'.static::$unreserved.static::$subDelims.':@?]|\%[A-Fa-f0-9]{2})*$/', $queryOrFragment)) {
+            throw new InvalidArgumentException(
+                "The query or the fragment component of the URI is not valid: {$queryOrFragment}!"
+            );
+        }
+
+        return $queryOrFragment;
     }
 }
