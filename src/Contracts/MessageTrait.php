1711b30335c6efbf2963b9abc880700bd9301ba6
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-23 19:08:28 +0300

    Update MessageTrait.php

--- src/Contracts/MessageTrait.php
+++ src/Contracts/MessageTrait.php
@@ -23,7 +23,7 @@ trait MessageTrait
     protected $headers = [];
 
     /**
-     * The message protocol version.
+     * The message HTTP protocol version.
      *
      * @var string
      */
@@ -42,6 +42,87 @@ trait MessageTrait
      * @var string
      */
     protected static $headerFieldValue = '/^[ \t]*(?:(?:[\x21-\x7e\x80-\xff](?:[ \t]+[\x21-\x7e\x80-\xff])?)|\r?\n[ \t]+)*[ \t]*$/';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getProtocolVersion()
+    {
+        return $this->protocolVersion;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function withProtocolVersion($version)
+    {
+        $new = clone $this;
+        $new->protocolVersion = $version;
+
+        return $new;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getHeaders()
+    {
+        $headers = [];
+
+        foreach ($this->headers as $header) {
+            $headers[$header['name']] = $header['values'];
+        }
+
+        return $headers;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function hasHeader($name)
+    {
+        return isset($this->headers[strtolower($name)]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getHeader($name)
+    {
+        $name = strtolower($name);
+
+        return isset($this->headers[$name]) ? $this->headers[$name]['values'] : [];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getHeaderLine($name)
+    {
+        return implode(', ', $this->getHeader($name));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function withHeader($name, $value)
+    {
+        $new = clone $this;
+        $new->setHeader($name, $value);
+
+        return $new;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function withAddedHeader($name, $value)
+    {
+        $new = clone $this;
+        $new->addHeader($name, $value);
+
+        return $new;
+    }
 
     /**
      * Set header to the message.
