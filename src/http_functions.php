1f1a4b422300c24185b222e9b4e4a992610fc1b1
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-24 18:15:43 +0300

    Create http_functions.php

---
+++ src/http_functions.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Parse an HTTP response string.
+ *
+ * @param string $response
+ * @return array
+ */
+function parse_response($response)
+{
+    $opts = [
+
+        'protocol' => null,
+        'status' => null,
+        'headers' => [],
+        'body' => null
+
+    ];
+
+    
+}
