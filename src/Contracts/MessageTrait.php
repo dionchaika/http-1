b145d5d42d79601531e158e468ec1bb53cc29746
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-24 15:40:49 +0300

    Update MessageTrait.php

--- src/Contracts/MessageTrait.php
+++ src/Contracts/MessageTrait.php
@@ -10,5 +10,5 @@ trait MessageTrait
     protected static $token = '/^[!#$%&\'*+\-.^_`|~0-9A-Za-z]+$/';
 
     /** @var string @see https://tools.ietf.org/html/rfc7230#section-3.2 */
-    protected static $header = '/^[ \t]*(?:(?:[\x21-\x7e\x80-\xff](?:[ \t]+[\x21-\x7e\x80-\xff])?)|\r?\n[ \t]+)*[ \t]*$/';
+    protected static $header = '/^[ \t]*(?:(?:[\x21-\x7e\x80-\xff](?:[ \t]+[\x21-\x7e\x80-\xff])?)|\r\n[ \t]+)*[ \t]*$/';
 }
