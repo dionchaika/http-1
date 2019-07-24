062adf0494af5b4ac7b9fa3949dcecc98d5e544e
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-24 14:43:05 +0300

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -39,7 +39,53 @@ function filter_uri_port($port)
         return $port;
     }
 
-    trigger_error('TCP or UDP port must be between 1 and 65535.');
+    trigger_error('TCP or UDP port MUST be between 1 and 65535.');
 
     throw new InvalidArgumentException("URI port component is not valid: {$port}!");
+}
+
+/**
+ * Filter a URI path component as described in the "RFC 3986".
+ *
+ * @see https://tools.ietf.org/html/rfc3986#section-3.3
+ *
+ * @param string $path
+ * @param string $scheme Optional URI scheme component.
+ * @param string $authority Optional URI authority component.
+ * @return string
+ * @throws InvalidArgumentException
+ */
+function filter_uri_path($path, $scheme = '', $authority = '')
+{
+    $valid = true;
+
+    if ('' !== $scheme && 0 === strpos($path, ':')) {
+        $valid = false;
+
+        trigger_error(
+            'Path of a URI with a scheme component MUST NOT start with a colon (:).'
+        );
+    }
+
+    if ('' !== $authority && 0 !== strpos($path, '/')) {
+        $valid = false;
+
+        trigger_error(
+            'Path of a URI with an authority component MUST be empty or start with a slash (/).'
+        );
+    } else if ('' === $authority && 0 === strpos($path, '//')) {
+        $valid = false;
+
+        trigger_error(
+            'Path of a URI without an authority component MUST NOT start with two slashes (//).'
+        );
+    }
+
+    $pattern = '/^(?:[\/A-Za-z0-9\-._~!$&\'()*+,;=:@]|\%[A-Fa-f0-9]{2})*$/';
+
+    if ($valid && ('' === $path || '/' === $path || preg_match($pattern, $path))) {
+        return $path;
+    }
+
+    throw new InvalidArgumentException("URI path component is not valid: {$path}!");
 }
