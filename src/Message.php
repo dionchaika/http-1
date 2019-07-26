abf3c1f37e2c74f70b33931284a102ec67623adb
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-26 16:08:25 +0300

    Update Message.php

--- src/Message.php
+++ src/Message.php
@@ -12,4 +12,13 @@ abstract class Message implements MessageInterface
 {
     const TOKEN = '/^[!#$%&\'*+\-.^_`|~0-9A-Za-z]+$/';
     const FIELD_VALUE = '/^[ \t]*(?:(?:[\x21-\x7e\x80-\xff](?:[ \t]+[\x21-\x7e\x80-\xff])?)|\r\n[ \t]+)*[ \t]*$/';
+
+    /** @var StreamInterface */
+    protected $body;
+
+    /** @var array */
+    protected $headers = [];
+
+    /** @var string */
+    protected $protocolVersion = '1.1';
 }
