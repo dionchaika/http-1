85b37cacf25f5b4ce7b2d4dc4695a9a40d872b5a
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-26 14:15:26 +0300

    Update Uri.php

--- src/Uri.php
+++ src/Uri.php
@@ -35,4 +35,43 @@ class Uri implements UriInterface
 
     /** @var string */
     protected $fragment = '';
+
+    /**
+     * Percent-encode a URI path component.
+     * This method DOES NOT double-encode any characters.
+     *
+     * @see https://tools.ietf.org/html/rfc3986#section-2
+     * @see https://tools.ietf.org/html/rfc3986#section-3.3
+     *
+     * @param string $path URI path component.
+     * @return string
+     */
+    protected static function percentEncodePath($path)
+    {
+        $pattern = '/(?:[\/'.self::UNRESERVED.self::SUB_DELIMS.':@%]++|\%(?![A-Za-z0-9]{2}))/';
+
+        return preg_replace_callback($pattern, function ($matches) {
+            return rawurlencode($matches[0]);
+        }, $path);
+    }
+
+    /**
+     * Percent-encode a URI query or fragment component.
+     * This method DOES NOT double-encode any characters.
+     *
+     * @see https://tools.ietf.org/html/rfc3986#section-2
+     * @see https://tools.ietf.org/html/rfc3986#section-3.4
+     * @see https://tools.ietf.org/html/rfc3986#section-3.5
+     *
+     * @param string $path URI query or fragment component.
+     * @return string
+     */
+    protected static function percentEncodeQueryOrFragment($queryOrFragment)
+    {
+        $pattern = '/(?:[\/'.self::UNRESERVED.self::SUB_DELIMS.':@?%]++|\%(?![A-Za-z0-9]{2}))/';
+
+        return preg_replace_callback($pattern, function ($matches) {
+            return rawurlencode($matches[0]);
+        }, $queryOrFragment);
+    }
 }
