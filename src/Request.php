a3488168a5a4f0c2afca942b9206e2b309ea59fe
The Lazy PHP Framework <52961186+thelazyphp@users.noreply.github.com>
2019-07-26 17:03:30 +0300

    Create Request.php

---
+++ src/Request.php
@@ -0,0 +1,170 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lazy\Http;
+
+use InvalidArgumentException;
+use Psr\Http\Message\UriInterface;
+use Psr\Http\Message\RequestInterface;
+
+class Request extends Message implements RequestInterface
+{
+    const HOST_HEADER = 'Host';
+
+    /** @var array */
+    protected static $standartMethods = [
+
+        'GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE'
+
+    ];
+
+    /** @var UriInterface */
+    protected $uri;
+
+    /** @var string */
+    protected $method;
+
+    /** @var mixed */
+    protected $requestTarget;
+
+    /**
+     * Filter an HTTP method.
+     *
+     * @param string $method
+     * @return string
+     * @throws InvalidArgumentException
+     */
+    protected static function filterMethod($method)
+    {
+        if (
+            in_array($method, static::$standartMethods) ||
+            preg_match(self::TOKEN, $method)
+        ) {
+            return $method;
+        }
+
+        throw new InvalidArgumentException("Invalid HTTP method: {$method}!");
+    }
+
+    /**
+     * Get the "Host" header value from the given URI.
+     *
+     * @param UriInterface $uri
+     * @return string
+     */
+    protected static function getHost(UriInterface $uri)
+    {
+        $host = $uri->getHost();
+
+        if ('' !== $host) {
+            $port = $uri->getPort();
+
+            if (null !== $port) {
+                $host .= ':'.$port;
+            }
+        }
+
+        return $host;
+    }
+
+    /**
+     * Initializes a new request instance.
+     *
+     * @param string $method
+     * @param UriInterface|string $uri
+     *
+     * @throws InvalidArgumentException
+     */
+    public function __construct($method, $uri)
+    {
+        $this->method = static::filterMethod($method);
+
+        if (is_string($uri)) {
+            $uri = new Uri($uri);
+        }
+
+        $this->uri = $uri;
+        $this->setHost($uri);
+    }
+
+    public function getRequestTarget()
+    {
+        if (null !== $this->requestTarget) {
+            return (string) $this->requestTarget;
+        }
+
+        $uri = $this->getUri();
+        $query = $uri->getQuery();
+
+        $requestTarget = '/'.ltrim($uri->getPath(), '/');
+
+        if ('' !== $query) {
+            $requestTarget .= '?'.$query;
+        }
+
+        return $requestTarget;
+    }
+
+    public function withRequestTarget($requestTarget)
+    {
+        $request = clone $this;
+        $request->requestTarget = $requestTarget;
+
+        return $request;
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function withMethod($method)
+    {
+        $request = clone $this;
+        $request->method = static::filterMethod($method);
+
+        return $request;
+    }
+
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    public function withUri(UriInterface $uri, $preserveHost = false)
+    {
+        $request = clone $this;
+        $request->uri = $uri;
+
+        if (! $preserveHost || '' === $this->getHeaderLine(self::HOST_HEADER)) {
+            $request->setHost($uri);
+        }
+
+        return $request;
+    }
+
+    /**
+     * Set the "Host" header from the given URI.
+     *
+     * @param UriInterface $uri
+     * @return void
+     */
+    protected function setHost(UriInterface $uri)
+    {
+        $host = static::getHost($uri);
+
+        if ('' !== $host) {
+            $this->headers = [
+
+                'host' => [
+
+                    'name' => self::HOST_HEADER,
+                    'values' => static::filterHeaderValue($host)
+
+                ]
+
+            ] + $this->headers;
+        }
+    }
+}
